36a957cc93c22147c9fe2f57d6b51164e746bc77
Shane <shane@shanerice.me>
2019-07-26 14:46:48 -0700

    Added some pages and lorem ipsum text

--- app/home/urls.py
+++ app/home/urls.py
@@ -6,4 +6,8 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home_view, name='index'),
+    path('about/', views.about_view, name='about'),
+    path('rfw1/', views.rfw1_view, name='rfw1'),
+    path('rfw2/', views.rfw2_view, name='rfw2'),
+    path('videos/', views.videos_view, name='videos'),
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -7,3 +7,23 @@ logger = logging.getLogger('app')
 def home_view(request):
     #  View: /
     return render(request, 'home.html')
+
+
+def about_view(request):
+    #  View: /videos/
+    return render(request, 'about.html')
+
+
+def rfw1_view(request):
+    #  View: /videos/
+    return render(request, 'rfw1.html')
+
+
+def rfw2_view(request):
+    #  View: /videos/
+    return render(request, 'rfw2.html')
+
+
+def videos_view(request):
+    #  View: /videos/
+    return render(request, 'videos.html')
